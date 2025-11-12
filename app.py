606bea7c54e47135555ace434a0239ed4552f254
DBPF-BioInf <daniel.bpereira.filho@gmail.com>
2025-11-12 15:10:11 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, send_file, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+
+@app.route('/curriculo')
+def curriculo():
+    return render_template('curriculo.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/habilidades')
+def habilidades():
+    return render_template('habilidades.html')
+
+@app.route('/baixar_curriculo')
+def baixar_curriculo():
+    return send_file('static/Daniel_Barbosa_Curriculo.pdf', as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True) #noqa
